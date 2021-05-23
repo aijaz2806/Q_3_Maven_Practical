a2f2e2bd3b2543ae6a80a985e5e1fac58ed417c2
AIJAZ <siyahiaijaz121016@gmail.com>
2021-05-23 12:44:00 +0530

    commit
    git commit

---
+++ src/main/java/com/example/Q_3_Maven_Practical/Q3MavenPracticalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Q_3_Maven_Practical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q3MavenPracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q3MavenPracticalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Q_3_Maven_Practical/Q3MavenPracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Q_3_Maven_Practical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q3MavenPracticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
